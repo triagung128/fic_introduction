5df3893215f03820348f411991e58b000e570ef7
triagung128 <triagung128@gmail.com>
2022-10-28 09:00:34 +0700

    initial commit

---
+++ bin/fic_introduction.dart
@@ -0,0 +1,5 @@
+import 'package:fic_introduction/fic_introduction.dart' as fic_introduction;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fic_introduction.calculate()}!');
+}
---
+++ test/fic_introduction_test.dart
@@ -0,0 +1,8 @@
+import 'package:fic_introduction/fic_introduction.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
